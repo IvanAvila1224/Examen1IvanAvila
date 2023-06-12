c3b07694da78d2dc78dd4d1b8edc7e4dc51d2223
IvanAvila1224 <magista1224@gmail.com>
2023-06-12 10:10:44 -0600

    crud update

---
+++ src/main/java/org/uv/Examen1IvanAM/Examen1IvanAmApplication.java
@@ -0,0 +1,14 @@
+package org.uv.Examen1IvanAM;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen1IvanAmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen1IvanAmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/uv/Examen1IvanAM/Examen1IvanAmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.Examen1IvanAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen1IvanAmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
